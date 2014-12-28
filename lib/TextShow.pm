9b2d1004bb312e28e41d4409083032c26e382487
Labbit <labbit@users.noreply.github.com>
2014-12-28 17:19:34 +0300

    Update TextShow.pm

--- lib/TextShow.pm
+++ lib/TextShow.pm
@@ -8,6 +8,7 @@ has 'dbconn' => sub {
 
 	my $conn = DBIx::Connector->new( 'dbi:mysql:dbname=sometest;', 'someus', 'Cho8yi4G', {RaiseError => 1, AutoCommit => 0});
 	my $dbh = $conn->dbh;
+	$dbh->do('set names utf8');	
 
 	return $conn;
 };
@@ -22,14 +23,17 @@ sub startup {
 	$r->get('/:id' => [id => qr/\d+/])->to('show#someone', id => 2);
 	$r->get('/tag/:tag')->to('show#taglist');
 
+	
 	# admin part
-	# my $rn = $r->bridge('/admin')->to('admin#check');	
-	# $r->get('/form')->to('admin#auth');
-	# $r->get('/login')->to('admin#login');
-	# $r->get('/logout')->to('admin#logout');
-	# $r->get('/admin/')->to('admin#list');
-	# $r->get('/admin/:id/edit' => [id => qr/\d+/])->to('admin#edit');	
-	# $r->get('/admin/:id/preview' => [id => qr/\d+/])->to('admin#someone');
+	$r->get('/form')->to('admin#auth', mymessage=> 'login, please');
+	$r->get('/login')->to('admin#login');
+	$r->get('/logout')->to('admin#logout');
+
+	my $rn = $r->bridge('/admin')->to('admin#check');	
+	$rn->route ->via('get')->to('admin#list');
+	$rn->get('/:id/edit' => [id => qr/\d+/])->to('admin#edit');	
+	$rn->get('/:id/preview' => [id => qr/\d+/])->to('admin#preview');	
+	$rn->post('/:id/save' => [id => qr/\d+/])->to('admin#update');	
 
 }
 
